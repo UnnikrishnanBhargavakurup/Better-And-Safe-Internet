9e023ca00db949ba98f5c9e2568259b206652a36
unnikrishnan <unnikrishnan.b@landmarkgroup.in>
2017-09-26 14:33:12 +0530

    fixes in addign Content-Filter header

--- background.js
+++ background.js
@@ -1,33 +1,59 @@
-var filter = {};
-/*
+var filter = {
+  "disturbing": false,
+  "violence": false,
+  "offensive": false,
+  "explicit": false,
+  "notification" : false
+};
+
 chrome.storage.sync.get("filter", function(data) {
-  $scope.filter = data.filter;
+  filter = data.filter || {
+    "disturbing": false,
+    "violence": false,
+    "offensive": false,
+    "explicit": false,
+    "notification" : false
+  };
 });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
   chrome.storage.sync.get("filter", function(data) {
-    $scope.filter = data.filter;
+    filter = data.filter;
   });
 });
-*/
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
-    if(filter != '') {
+    var filterValues = getFilterValues();
+    if(filterValues != '') {
       var found = false;
       for (var i = 0; i < details.requestHeaders.length; ++i) {
         if (details.requestHeaders[i].name === 'Content-Filter') {
-          details.requestHeaders[i] = filter;
+          details.requestHeaders[i] = filterValues;
           found = true;
           break;
         }
       }
       if(!found) {
-        details.requestHeaders.push({"Content-Filter" : filter})
+        details.requestHeaders.push({
+          "name": "Content-Filter",
+          "value": filterValues
+        })
       }
-      return {requestHeaders: details.requestHeaders};
     }
+    return {requestHeaders: details.requestHeaders};
   },
   {urls: ["<all_urls>"]},
   ["blocking", "requestHeaders"]
 );
+
+function getFilterValues() {
+  var filterValues = ''
+  for (var key in filter) {
+    if (filter.hasOwnProperty(key) && filter[key] === true) {
+      filterValues += key + ",";
+    }
+  }
+  return filterValues.replace(/,(\s+)?$/, '');
+}
 
